787df2d144046b47587ae1ca3d152a8e8fd255e8
Pavel Drbálek <pavel.drbalek@gmail.com>
2013-10-18 12:44:55 +0200

    Úprava přidávání a výpisu kategorií

--- wp-content/plugins/budkutil/budkutil.php
+++ wp-content/plugins/budkutil/budkutil.php
@@ -12,7 +12,12 @@ Version: 0.1
 Author URI: http://webartmedia.cz
 
 */
+$exists = $wpdb->get_var("SELECT COUNT(1) FROM $wpdb->terms WHERE active!=''");
 
+if(is_null($exists))
+{
+    $wpdb->query("ALTER TABLE $wpdb->terms ADD active INT NOT NULL DEFAULT 1");
+}
 
 function pridat_pole_kategorie($taxonomy) {
   $pole = '
@@ -25,6 +30,53 @@ function pridat_pole_kategorie($taxonomy) {
 	echo $pole;
 }
 add_filter("product_cat_add_form_fields", "pridat_pole_kategorie");
+
+function pridat_sloupec_kategorie( $columns ) {
+    $new_columns = array();
+    $new_columns['cb'] = $columns['cb'];
+    $new_columns['active'] = __( 'Aktivní', 'woocommerce' );
+
+    unset( $columns['cb'] );
+
+    return array_merge( $new_columns, $columns );
+}
+
+add_filter( 'manage_edit-product_cat_columns', 'pridat_sloupec_kategorie' );
+
+function pridat_udaj_kategorie( $columns, $column, $id ) {
+    global $woocommerce;
+    global $wpdb;
+
+    if ( $column == 'active' ) {
+
+        if($wpdb->get_var("SELECT active FROM $wpdb->terms WHERE term_id='$id'") == 1)
+            $text = "Aktivní";
+        else
+            $text = "Neaktivní";
+
+        $columns .= $text;
+
+    }
+
+    return $columns;
+}
+
+add_filter( 'manage_product_cat_custom_column', 'pridat_udaj_kategorie', 10, 3 );
+
+
+function ulozit_udaj_kategorie( $term_id, $tt_id, $taxonomy ) {
+    global $wpdb;
+
+    if ( isset( $_POST['zobr'] ) ){
+    
+        $wpdb->query("UPDATE $wpdb->terms SET active='".(($_POST['zobr'] == 1) ? 0 : 1)."' WHERE term_id='$term_id'");
+    }
+}
+
+add_action( 'created_term', 'ulozit_udaj_kategorie', 10,3 );
+add_action( 'edit_term', 'ulozit_udaj_kategorie', 10,3 );
+
+
 
 
 if (!class_exists("budkutil_admin_menu")) {
