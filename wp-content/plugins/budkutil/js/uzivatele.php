81def6576f694de37902197b8c1a8a19c76f6013
Pavel Drbálek <pavel.drbalek@gmail.com>
2013-11-05 17:16:26 +0100

    Generování výsledků pro našeptávač

--- wp-content/plugins/budkutil/js/uzivatele.php
+++ wp-content/plugins/budkutil/js/uzivatele.php
@@ -1,12 +1,18 @@
 <?
-require_once('../../../../../wp-load.php');
-$sql = "SELECT * FROM $wpdb->users WHERE user_email REGEXP '".$_GET['q']."' OR ID REGEXP '".$_GET['q']."' OR user_nicename REGEXP '".$_GET['q']."' OR user_login REGEXP '".$_GET['q']."' OR user_email REGEXP '".$_GET['q']."' OR display_name REGEXP '".$_GET['q']."'";
-$uzivatele = $wpdb->get_results($sql);
+$umisteni = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+require_once( $umisteni[0] . 'wp-load.php' );
 
-$answer = array();
-foreach ($uzivatele as $uzivatel) {
-	array_push($answer, array("id"=>$uzivatel->ID,"text"=>$uzivatel->display_name));
+if(is_user_logged_in())
+{
+	$q = sanitize_text_field($_GET['q']);
+
+	$sql = "SELECT * FROM $wpdb->users WHERE user_email REGEXP '".$q."' OR ID REGEXP '".$q."' OR user_nicename REGEXP '".$q."' OR user_login REGEXP '".$q."' OR user_email REGEXP '".$q."' OR display_name REGEXP '".$q."'";
+	$uzivatele = $wpdb->get_results($sql);
+
+	$answer = array();
+	foreach ($uzivatele as $uzivatel)
+		array_push($answer, array("id"=>$uzivatel->ID,"text"=>$uzivatel->display_name));
+
+	echo json_encode($answer);
 }
-
-echo json_encode($answer);
 ?>
