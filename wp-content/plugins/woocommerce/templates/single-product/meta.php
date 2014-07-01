6a965bf9a1fd218305fa6c3580f96129ac48ce1b
David Šuška <me@webni.cz>
2014-07-01 15:44:37 +0200

    Design changes

--- wp-content/plugins/woocommerce/templates/single-product/meta.php
+++ wp-content/plugins/woocommerce/templates/single-product/meta.php
@@ -24,9 +24,9 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 	<?php endif; ?>
 
-	<?php echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . ' ', '.</span>' ); ?>
+	<?php echo $product->get_categories( ' ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . ' ', '</span>' ); ?>
 
-	<?php echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $tag_count, 'woocommerce' ) . ' ', '.</span>' ); ?>
+	<?php echo $product->get_tags( ' ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $tag_count, 'woocommerce' ) . ' ', '</span>' ); ?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
--- wp-content/plugins/woocommerce/templates/single-product/title.php
+++ wp-content/plugins/woocommerce/templates/single-product/title.php
@@ -11,3 +11,4 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
 <h1 itemprop="name" class="product_title entry-title"><?php the_title(); ?></h1>
+<h3 class="author"><a href="#uzivateluv-ucet" class="name">Jméno Autora</a></h3>
